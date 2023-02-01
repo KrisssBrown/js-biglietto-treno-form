402cb02253fc00b6bb63473cf8263d21f13ba396
Krisssbrown <cristianbruno.cs@gmail.com>
2023-02-01 17:07:28 +0100

    START

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+const prezzoPerKm = 0.21
+const element = document.getElementById('totKm')
+
+//prendere value kilometri da input text
+let distanza = parseFloat()
+
+//prendere value select che indica lo sconto
